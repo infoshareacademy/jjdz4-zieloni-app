5e28bb456736dff5f97a0307f273369c631ed8f7
michalstasinski <michal.stasinski80@gmail.com>
2017-09-26 15:40:54 -0400

    konfiguracja projektu

---
+++ src/main/java/com/infoshareacademy/zieloni/App.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.zieloni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
