f5213a47d4037b10bba7c248759229d3d639f9dc
Marcin Kruszyński <github@kruszynscy.pl>
2017-10-15 19:03:45 +0200

    Dodane pola do klasy Event

--- src/main/java/com/infoshareacademy/zieloni/Event.java
+++ src/main/java/com/infoshareacademy/zieloni/Event.java
@@ -1,4 +1,11 @@
 package com.infoshareacademy.zieloni;
 
+import java.util.Date;
+
 public class Event {
+    private Date startTime; // Czas rozpoczęcia wydarzenia
+    private Date endTime; // Czas zakończenia wydarzenia
+    private String uid; // UID
+    private String location; // Miejsce wydarzenia
+    private String summary; // Opis wydarzenia
 }
