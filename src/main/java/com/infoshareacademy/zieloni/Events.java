bf7d2a334a81a377c9fafa1ec156f29a11905ff7
Marcin Kruszyński <github@kruszynscy.pl>
2017-10-15 19:30:29 +0200

    Dodanie szkieletu metody wczytującej wydarzenia z pliku ical

--- src/main/java/com/infoshareacademy/zieloni/Events.java
+++ src/main/java/com/infoshareacademy/zieloni/Events.java
@@ -4,4 +4,8 @@ import java.util.ArrayList;
 
 public class Events {
     private ArrayList<Event> events = new ArrayList<>();
+
+    public void loadEvents(){
+        // metoda wczytująca wydarzenia z pliku ical
+    }
 }
