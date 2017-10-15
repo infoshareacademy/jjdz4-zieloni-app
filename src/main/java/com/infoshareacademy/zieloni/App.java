29368226ae374c348e589226e3fff68aae2827a1
dziadborowy <adam.chudeusz@gmail.com>
2017-10-15 20:46:17 +0200

    loading ical method added

--- src/main/java/com/infoshareacademy/zieloni/App.java
+++ src/main/java/com/infoshareacademy/zieloni/App.java
@@ -1,13 +1,19 @@
 package com.infoshareacademy.zieloni;
 
+import net.fortuna.ical4j.data.ParserException;
+import java.io.IOException;
+import java.text.ParseException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main( String[] args ) throws ParseException, ParserException, IOException {
+        Events events = new Events();
+        events.loadEvents();
+        System.out.println(events.getEvents().size());
     }
 }
--- src/main/java/com/infoshareacademy/zieloni/Events.java
+++ src/main/java/com/infoshareacademy/zieloni/Events.java
@@ -1,20 +1,45 @@
 package com.infoshareacademy.zieloni;
 
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
+import net.fortuna.ical4j.model.Property;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Iterator;
 
 public class Events {
-    private ArrayList<Event> events = new ArrayList<>();
-
-    public void loadEvents(){
-        // metoda wczytująca wydarzenia z pliku ical
-        // ..
-
-
-//        add();
+    public ArrayList<Event> getEvents() {
+        return events;
     }
 
-    public void add(Date startTime, Date endTime, String uid, String location, String summary) {
-        events.add(new Event(startTime, endTime, uid, location, summary));
+    private ArrayList<Event> events = new ArrayList<>();
+
+    public void loadEvents() throws IOException, ParserException, ParseException {
+        FileInputStream icalFile = new FileInputStream("kalendarz.ics");
+        CalendarBuilder builder = new CalendarBuilder();
+        Calendar calendar = builder.build(icalFile);
+
+        for (Iterator  i = calendar.getComponents().iterator(); i.hasNext();) {
+            Component component = (Component) i.next();
+
+            String eventStart = (String.valueOf(component.getProperty(Property.DTSTART).getValue().replace("T", "").replace("Z", "")));
+            String eventEnd = (String.valueOf(component.getProperty(Property.DTEND).getValue().replace("T", "").replace("Z", "")));
+            String eventLocation = (String.valueOf(component.getProperty(Property.LOCATION).getValue()));
+            String eventUID = (String.valueOf(component.getProperty(Property.UID).getValue()));
+            String eventSummary = (String.valueOf(component.getProperty(Property.SUMMARY).getValue()));
+            add(eventStart, eventEnd, eventUID, eventLocation, eventSummary);
+        }
+
+    }
+
+    public void add(String startTime, String endTime, String uid, String location, String summary) throws ParseException {
+        SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss");
+        events.add(new Event(sf.parse(startTime), sf.parse(endTime), uid, location, summary));
     }
 }
