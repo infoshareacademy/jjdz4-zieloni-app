e54193f9af38990edc8d2e0c94428c0ab2f0ad13
akosala <akosala@wp.pl>
2017-10-19 22:11:49 +0200

    find bus class

--- src/main/java/com/infoshareacademy/zieloni/SerchBus.java
+++ src/main/java/com/infoshareacademy/zieloni/SerchBus.java
@@ -6,43 +6,37 @@ import com.infoshareacademy.zieloni.Model.Bus;
 import java.util.ArrayList;
 
 public class SerchBus{
-    public static String busNumber1(String streetBus){
-        String number1= new String(String.valueOf(busNumber1(streetBus)));
-        ArrayList<Bus> busDB = new ArrayList<Bus>();
-       // ArrayList<Bus> busDB1 = new ArrayList<Bus>();
 
+    public static  ArrayList<Bus>search(String nazwaUlicy) {
+        ArrayList<Bus> nrbus = new ArrayList<Bus>();
+ //nrbus=null;
+        ArrayList<Bus> busDB = new ArrayList<Bus>();
         int i=0;
         for (i = 0; i <= busDB.size(); i++) {
-
-
-            //System.out.println("Autobus o numerze " + busDB.get(i).getBusNumber());
-
-            //System.out.println("Zatrzymuje sie na ulicach"+  street);
             int k = 0;
-            try {
+            try{
                 for (k = 0; k <= busDB.get(i).getBusStopVariant1().size(); k++) {
                     String busStop = (busDB.get(i).getBusStopVariant1().get(k).getNameOfBasStop());
                     // String busStop = (busDB.get(k).getBusStopVariant1().get(i).getNameOfBasStop());
-                    if (busStop.equalsIgnoreCase(streetBus)) {
+                    if (busStop.equalsIgnoreCase(nazwaUlicy)) {
                         //System.out.println("    " + busDB.get(i).getBusStopVariant1().get(k).getNameOfBasStop());
-                        number1 = busDB.get(i).getBusNumber();
-                        k = 0;
+                        System.out.println("Autobus o numerze " + busDB.get(i).getBusNumber());
+                        k=0;
                         break;
                     }
                 }
-            }
-            catch (IndexOutOfBoundsException e){
+            } catch (IndexOutOfBoundsException e){
                 // System.out.println(" numer i ="+i+", numer k = "+k);
                 //System.out.println(k + "    " + busDB.get(i).getBusStopVariant1().get(k).getNameOfBasStop());
 
             }
+            // System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
         }
+        System.out.println("********************************************************************");
+return nrbus;
 
-
-
-
-
-        return number1;
     }
+
+
 }
 
