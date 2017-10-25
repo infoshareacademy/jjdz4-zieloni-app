5abb2c9aec4c0d8bff887873b4877bf211f32492
Marcin Kruszyński <github@kruszynscy.pl>
2017-10-25 12:41:21 +0200

    Replace Date by LocalDateTime, Replace Iterator by foreach loop,

--- src/main/java/com/infoshareacademy/zieloni/Event.java
+++ src/main/java/com/infoshareacademy/zieloni/Event.java
@@ -1,26 +1,26 @@
 package com.infoshareacademy.zieloni;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class Event {
-    private Date startTime; // Czas rozpoczęcia wydarzenia
-    private Date endTime; // Czas zakończenia wydarzenia
+    private LocalDateTime startTime; // Czas rozpoczęcia wydarzenia
+    private LocalDateTime endTime; // Czas zakończenia wydarzenia
     private String uid; // UID
     private String location; // Miejsce wydarzenia
     private String summary; // Opis wydarzenia
 
-    public Event(Date startTime, Date endTime, String uid, String location, String summary) {
+    public Event(LocalDateTime startTime, LocalDateTime endTime, String uid, String location, String summary) {
         this.startTime = startTime;
         this.endTime = endTime;
         this.uid = uid;
         this.location = location;
         this.summary = summary;
     }
-    public Date getStartTime() {
+    public LocalDateTime getStartTime() {
         return startTime;
     }
 
-    public Date getEndTime() {
+    public LocalDateTime getEndTime() {
         return endTime;
     }
 
--- src/main/java/com/infoshareacademy/zieloni/Events.java
+++ src/main/java/com/infoshareacademy/zieloni/Events.java
@@ -5,13 +5,13 @@ import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.Property;
-
+import net.fortuna.ical4j.model.component.CalendarComponent;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Events {
     public ArrayList<Event> getEvents() {
@@ -25,9 +25,8 @@ public class Events {
         CalendarBuilder builder = new CalendarBuilder();
         Calendar calendar = builder.build(icalFile);
 
-        for (Iterator  i = calendar.getComponents().iterator(); i.hasNext();) {
-            Component component = (Component) i.next();
-
+        for (CalendarComponent calendarComponent : calendar.getComponents()) {
+            Component component = calendarComponent;
             String eventStart = (String.valueOf(component.getProperty(Property.DTSTART).getValue().replace("T", "").replace("Z", "")));
             String eventEnd = (String.valueOf(component.getProperty(Property.DTEND).getValue().replace("T", "").replace("Z", "")));
             String eventLocation = (String.valueOf(component.getProperty(Property.LOCATION).getValue()));
@@ -35,11 +34,13 @@ public class Events {
             String eventSummary = (String.valueOf(component.getProperty(Property.SUMMARY).getValue()));
             add(eventStart, eventEnd, eventUID, eventLocation, eventSummary);
         }
-
     }
 
     public void add(String startTime, String endTime, String uid, String location, String summary) throws ParseException {
-        SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHHmmss");
-        events.add(new Event(sf.parse(startTime), sf.parse(endTime), uid, location, summary));
+        events.add(new Event(LocalDateTime.parse(startTime, DateTimeFormatter.ofPattern("yyyyMMddHHmmss")),
+                LocalDateTime.parse(endTime, DateTimeFormatter.ofPattern("yyyyMMddHHmmss")),
+                uid,
+                location,
+                summary));
     }
 }
