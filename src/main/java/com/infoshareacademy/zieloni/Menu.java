b763c8bb0cea01432fb77e822d3d92849a9fd4f7
Marcin Kruszyński <github@kruszynscy.pl>
2017-10-25 12:12:50 +0200

    Add method to clear linux console

--- src/main/java/com/infoshareacademy/zieloni/Menu.java
+++ src/main/java/com/infoshareacademy/zieloni/Menu.java
@@ -2,6 +2,19 @@ package com.infoshareacademy.zieloni;
 
 public class Menu {
 
+    /**
+     * Clears linux console
+     */
+    public static void clearConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    /**
+     * Displays all events passed in a collection
+     *
+     * @param events collection of events to display
+     */
     public static void displayEvents(Events events) {
         if (events.getEvents().size() != 0) {
 
@@ -12,9 +25,9 @@ public class Menu {
                 System.out.println("-----------------------------------------------------");
                 System.out.println("Wydarzenie nr: " + i);
                 System.out.println("Początek wydarzenia: \t" + event.getStartTime());
-                System.out.println("Koniec wydarzenia: \t\t" + event.getEndTime());
+                System.out.println("Koniec wydarzenia: \t" + event.getEndTime());
                 System.out.println("Miejsce wydarzenia: \t" + event.getLocation());
-                System.out.println("Opis wydarzenia: \t\t" + event.getSummary());
+                System.out.println("Opis wydarzenia: \t" + event.getSummary());
                 i++;
             }
         } else {
