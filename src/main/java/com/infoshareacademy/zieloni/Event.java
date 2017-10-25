530760d4f2b5324bc8a209ec57dea0c12acb78e6
marcin.kruszynski <github@kruszyscy.pl>
2017-10-25 21:27:25 +0200

    Using Lombok Project

--- src/main/java/com/infoshareacademy/zieloni/Event.java
+++ src/main/java/com/infoshareacademy/zieloni/Event.java
@@ -1,38 +1,21 @@
 package com.infoshareacademy.zieloni;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
-public class Event {
-    private LocalDateTime startTime; // Czas rozpoczęcia wydarzenia
-    private LocalDateTime endTime; // Czas zakończenia wydarzenia
-    private String uid; // UID
-    private String location; // Miejsce wydarzenia
-    private String summary; // Opis wydarzenia
+class Event {
+    @Getter private LocalDateTime startTime; // Czas rozpoczęcia wydarzenia
+    @Getter private LocalDateTime endTime; // Czas zakończenia wydarzenia
+    @Getter private String uid; // UID
+    @Getter private String location; // Miejsce wydarzenia
+    @Getter private String summary; // Opis wydarzenia
 
-    public Event(LocalDateTime startTime, LocalDateTime endTime, String uid, String location, String summary) {
+    Event(LocalDateTime startTime, LocalDateTime endTime, String uid, String location, String summary) {
         this.startTime = startTime;
         this.endTime = endTime;
         this.uid = uid;
         this.location = location;
         this.summary = summary;
-    }
-    public LocalDateTime getStartTime() {
-        return startTime;
-    }
-
-    public LocalDateTime getEndTime() {
-        return endTime;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public String getSummary() {
-        return summary;
     }
 }
--- src/main/java/com/infoshareacademy/zieloni/Events.java
+++ src/main/java/com/infoshareacademy/zieloni/Events.java
@@ -1,11 +1,12 @@
 package com.infoshareacademy.zieloni;
 
+import lombok.Getter;
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.component.CalendarComponent;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.ParseException;
@@ -13,30 +14,25 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-public class Events {
-    public ArrayList<Event> getEvents() {
-        return events;
-    }
+class Events {
+    @Getter private ArrayList<Event> events = new ArrayList<>();
 
-    private ArrayList<Event> events = new ArrayList<>();
-
-    public void loadEvents() throws IOException, ParserException, ParseException {
+    void loadEvents() throws IOException, ParserException, ParseException {
         FileInputStream icalFile = new FileInputStream("kalendarz.ics");
         CalendarBuilder builder = new CalendarBuilder();
         Calendar calendar = builder.build(icalFile);
 
         for (CalendarComponent calendarComponent : calendar.getComponents()) {
-            Component component = calendarComponent;
-            String eventStart = (String.valueOf(component.getProperty(Property.DTSTART).getValue().replace("T", "").replace("Z", "")));
-            String eventEnd = (String.valueOf(component.getProperty(Property.DTEND).getValue().replace("T", "").replace("Z", "")));
-            String eventLocation = (String.valueOf(component.getProperty(Property.LOCATION).getValue()));
-            String eventUID = (String.valueOf(component.getProperty(Property.UID).getValue()));
-            String eventSummary = (String.valueOf(component.getProperty(Property.SUMMARY).getValue()));
+            String eventStart = (String.valueOf(calendarComponent.getProperty(Property.DTSTART).getValue().replace("T", "").replace("Z", "")));
+            String eventEnd = (String.valueOf(calendarComponent.getProperty(Property.DTEND).getValue().replace("T", "").replace("Z", "")));
+            String eventLocation = (String.valueOf(calendarComponent.getProperty(Property.LOCATION).getValue()));
+            String eventUID = (String.valueOf(calendarComponent.getProperty(Property.UID).getValue()));
+            String eventSummary = (String.valueOf(calendarComponent.getProperty(Property.SUMMARY).getValue()));
             add(eventStart, eventEnd, eventUID, eventLocation, eventSummary);
         }
     }
 
-    public void add(String startTime, String endTime, String uid, String location, String summary) throws ParseException {
+    private void add(String startTime, String endTime, String uid, String location, String summary) throws ParseException {
         events.add(new Event(LocalDateTime.parse(startTime, DateTimeFormatter.ofPattern("yyyyMMddHHmmss")),
                 LocalDateTime.parse(endTime, DateTimeFormatter.ofPattern("yyyyMMddHHmmss")),
                 uid,
