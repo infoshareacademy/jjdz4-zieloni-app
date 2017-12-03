7211c9afc6509b798962d7116f07e33fd8ccd34f
dziadborowy <adam.chudeusz@gmail.com>
2017-12-03 20:32:38 +0100

    init: jee dependencies and first servlet added

---
+++ planer-webapp/src/main/java/com/infoshareacademy/zieloni/Main.java
@@ -0,0 +1,7 @@
+package com.infoshareacademy.zieloni;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nie wyświetliłem żadnej strony ahahahahhahaaa!!!");
+    }
+}
