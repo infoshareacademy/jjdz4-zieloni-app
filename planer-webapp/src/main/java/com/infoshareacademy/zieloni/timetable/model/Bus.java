849228ec1c46bd1f960be90413bda6da4c8c0524
akosala <akosala@wp.pl>
2018-03-06 22:35:19 +0100

    commit servlet BusPromotion Order by name

--- planer-webapp/src/main/java/com/infoshareacademy/zieloni/timetable/model/Bus.java
+++ planer-webapp/src/main/java/com/infoshareacademy/zieloni/timetable/model/Bus.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 
 @NamedQueries({
         @NamedQuery(name = "getBusByName", query = "from Bus b where b.name=:name"),
-        @NamedQuery(name = "getAllBus", query = "from Bus"),
+        @NamedQuery(name = "getAllBus", query = "from Bus order by name"),
         @NamedQuery(name = "updateBus", query = "update Bus b  set b.name =:name, b.status=:status, b.type=:type where b.id=:id"),
 })
 public class Bus implements Serializable {
